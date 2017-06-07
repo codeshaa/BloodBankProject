670fec59f062fab6ca121ebae1b58ea9ea04d3aa
Shaanz <sharunjon@gmail.com>
2017-06-08 00:22:32 +1200

    updated details directory

---
+++ app/src/main/java/com/unitec/bloodbank/bloodbankproject/RequesterDetail.java
@@ -0,0 +1,13 @@
+package com.unitec.bloodbank.bloodbankproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RequesterDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_requester_detail);
+    }
+}
